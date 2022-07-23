0bb4661086f21db5f84f7cc9039dde08b027f05e
robin <ibots.robin@gmail.com>
2022-07-23 22:08:45 +0530

    react portfolio

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,30 @@
 // import React from 'react'
 import './header.css'
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class Header extends Component {
-  state = {  } 
-  render() { 
-    return (<div>Header</div>);
+render() { 
+    return (
+      <header>
+     <div
+        className='p-5 text-center bg-image'
+        style={{height: 500 }}
+      >
+        <div className='mask'>
+          <div className='d-flex justify-content-center align-items-center h-100'>
+            <div className='text-black'>
+              <h1 className='mb-3'>Heading</h1>
+              <h4 className='mb-3'>Subheading</h4>
+              <a className='btn btn-outline-light btn-lg' href='#!' role='button'>
+                Call to action
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+</header>
+   
+);
   }
 }
  
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import  ReactDOM  from "react-dom";
+import React from 'react';
 import App from './app';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 
 ReactDOM.render(<App/>,document.querySelector("#root"))
